faa8faf46f1bec8ff4901e13d548fe9b27fbe3bb
Your Name <you@example.com>
2026-04-11 12:42:16 +0530

    something changes like improve signup pages and more improvement

--- digital_campus/wsgi.py
+++ digital_campus/wsgi.py
@@ -8,6 +8,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Ensure the root project directory is in the Python path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
@@ -15,5 +21,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_campus.settings')
 
 application = get_wsgi_application()
 
-# Vercel requires a variable named 'app'
+# Vercel needs "app" variable
 app = application
